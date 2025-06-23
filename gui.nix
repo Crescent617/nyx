a71cc37568b207454c40d7a792e5138653e0d57f
Crescent617 <lihuaru617@outlook.com>
2025-06-24 03:26:39 +0800

    feat: input method

--- gui.nix
+++ gui.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, font, ... }:
 
 let cfg = config.nyx.gui;
 in
@@ -21,26 +21,66 @@ in
       nvidiaSettings = true;
     };
 
-    i18n.inputMethod.type.enabled = "fcitx5";
-    i18n.inputMethod.fcitx5.addons = with pkgs; [ 
-      fcitx5-chinese-addons
-      fcitx5-rime
-      fcitx5-qt
-      fcitx5-gtk
-    ];
+    # 启用 fcitx5 输入法
+    i18n.inputMethod = {
+      type = "fcitx5";
+      enable = true;
+      fcitx5.addons = with pkgs; [
+        fcitx5-chinese-addons
+        fcitx5-configtool # 图形配置工具
+        fcitx5-gtk
+        fcitx5-rime
+        fcitx5-nord
+      ];
+    };
 
-    programs.firefox.enable = true;
-    environment.systemPackages = with pkgs; [
-      # Fonts
+    # 安装中文字体
+    fonts.packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      wqy_zenhei
+      wqy_microhei
       nerd-fonts.jetbrains-mono
       nerd-fonts.caskaydia-cove
       maple-mono.truetype # Maple Mono (Ligature TTF unhinted)
       maple-mono.NF-CN-unhinted # Maple Mono NF CN (Ligature unhinted)
+    ];
 
-      # Apps
-      kitty
+    environment.sessionVariables = {
+      GTK_IM_MODULE = "fcitx";
+      QT_IM_MODULE = "fcitx";
+      XMODIFIERS = "@im=fcitx";
+    };
+
+    # Browser
+    programs.firefox.enable = true;
+    # Audio
+    services.pipewire = {
+      enable = true;
+      pulse.enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+    };
+
+    xdg.portal = {
+      enable = true;
+      extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
+      configPackages = [ pkgs.xdg-desktop-portal-wlr ];
+    };
+
+    environment.systemPackages = with pkgs; [
+      clipse # 剪贴板管理器
       fuzzel # 启动器
+      kitty
+      mako # 通知管理器
+      noti # 通知发送器
+      pavucontrol # 音量控制
+      pamixer # 控制音量（volume 模块用）
+      swaybg # 背景设置
       waybar # 状态栏
+      wl-clipboard # 用于在 Wayland 上复制粘贴
+      wlsunset # 夜间模式
+      xwayland-satellite # XWayland 支持
     ];
   };
 }
