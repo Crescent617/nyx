f36a6ff617fa69e6559236b8fc24f444f4ac326a
Crescent617 <lihuaru617@outlook.com>
2025-06-30 00:26:10 +0800

    enhance: gpu passthru

--- script/qemu_hooks.tmpl.sh
+++ script/qemu_hooks.tmpl.sh
@@ -1,14 +1,36 @@
 #!/usr/bin/env bash
 
+# This script handles GPU passthrough preparation and release events for QEMU virtual machines.
+# It is triggered by QEMU hooks and performs tasks such as stopping the display manager,
+# unbinding the GPU from the host system, and loading the VFIO kernel module for passthrough.
+# Additionally, it manages re-binding the GPU and restarting the display manager after use.
+#
+# Usage:
+#   qemu_hooks.tmpl.sh <guest_name> <event> <sub_event>
+#
+# Arguments:
+#   guest_name - Name of the guest virtual machine. (only when guest_name end with "pt")
+#   event      - Main event type (e.g., 'prepare' or 'release').
+#   sub_event  - Sub-event type (e.g., 'begin' or 'end').
+
 set -eu
 
 export PATH=/run/current-system/sw/bin:/usr/bin:$PATH
 
+# ==== CONFIG ====
+pci_devices=(
+  "pci_0000_01_00_0" # GPU
+  "pci_0000_01_00_1" # Audio
+  "pci_0000_01_00_2" # USB Controller
+  "pci_0000_01_00_3" # USB Controller
+)
+
+logfile="/tmp/qemu_hooks.log"
+
+# ==== Args ====
 GUEST_NAME="$1"
 EVENT="$2"
 SUB_EVENT="$3"
-
-logfile="/tmp/qemu_hooks.log"
 
 info() {
   echo "$(date +'%Y-%m-%d %H:%M:%S') [INFO] $1" | tee -a "$logfile"
@@ -57,10 +79,9 @@ if [[ $gpu_passthru == 1 && "$EVENT" == "prepare" && "$SUB_EVENT" == "begin" ]];
 
   # Unbind the GPU from display driver
   info "Unbinding GPU from display driver"
-  virsh nodedev-detach pci_0000_01_00_0
-  virsh nodedev-detach pci_0000_01_00_1
-  virsh nodedev-detach pci_0000_01_00_2
-  virsh nodedev-detach pci_0000_01_00_3
+  for device in "${pci_devices[@]}"; do
+    virsh nodedev-detach "$device"
+  done
 
   # Load VFIO Kernel Module
   modprobe vfio-pci
@@ -72,10 +93,9 @@ if [[ $gpu_passthru == 1 && "$EVENT" == "release" && "$SUB_EVENT" == "end" ]]; t
 
   # Re-Bind GPU to Nvidia Driver
   info "Stopping VFIO Kernel Module"
-  virsh nodedev-reattach pci_0000_01_00_3
-  virsh nodedev-reattach pci_0000_01_00_2
-  virsh nodedev-reattach pci_0000_01_00_1
-  virsh nodedev-reattach pci_0000_01_00_0
+  for ((i = ${#pci_devices[@]} - 1; i >= 0; i--)); do
+    virsh nodedev-reattach "${pci_devices[i]}"
+  done
 
   # Reload nvidia modules
   info "Reloading Nvidia Modules"
