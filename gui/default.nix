8cd421185bad6735803d7eb5b3516de0fcc058cb
Crescent617 <lihuaru617@outlook.com>
2026-08-15 20:22:48 +0800

    feat: add anydesk; nrs alias auto-updates nyx input

--- gui/default.nix
+++ gui/default.nix
@@ -139,6 +139,7 @@ in
       unityhub
       remmina
       # (preferUnstable "nomachine-client") # 暂时移除：官方下架了 9.5.7 安装包，等 nixpkgs 更新后再加回
+      anydesk
 
       nur.repos.xddxdd.baidunetdisk
       yomi-app
