f2fabf71644055a1edc83c7bbf03d0f809cf05e1
Crescent617 <lihuaru617@outlook.com>
2026-08-16 21:14:35 +0800

    chore: remove searxng service (replaced by docker-compose instance in ~/services)

--- services/default.nix
+++ services/default.nix
@@ -2,24 +2,8 @@
 
 with lib;
 
-let
-  configDir = "${./.}/etc";
-in
 {
   config = {
-    virtualisation.oci-containers.containers = {
-      searxng = {
-        image = "searxng/searxng:latest";
-        autoStart = true;
-        ports = [ "8081:8080" ];
-        volumes = [
-          "${configDir}/searxng:/etc/searxng"
-        ];
-        environment = {
-          BASE_URL = "http://localhost:8081/";
-        };
-      };
-    };
     services.envfs.enable = lib.mkDefault true;
   };
 }
